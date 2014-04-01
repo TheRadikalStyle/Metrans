d71f26f90a2e9733228c4ec797c7123add58527a
TheRadikalStyle <radikal_david@hotmail.com>
2014-03-31 23:33:02 -0600

    Several changes
    Added notification on statusbar when app starts

--- src/com/theradikalsoftware/metrans/MainActivity.java
+++ src/com/theradikalsoftware/metrans/MainActivity.java
@@ -2,15 +2,28 @@ package com.theradikalsoftware.metrans;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.view.Menu;
 import android.view.MenuItem;
 
+
+
 public class MainActivity extends Activity {
 
+	
+	/*private NotificationManager nm; //Variables al admin de notif.
+	private static final int ID_NOTIFICACION_CREAR = 1;*/
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		//nm = (NotificationManager)getSystemService(NOTIFICATION_SERVICE); //Declaracion de notificacion
+		//noti();
+		 
 	}
 
 	@Override
@@ -34,10 +47,28 @@ public class MainActivity extends Activity {
 	
 	/*****Funciones del menu de esta actividad*****/
 	private void Exit() {
+		
+		//nm.cancel(ID_NOTIFICACION_CREAR);
 		finish();
 	    android.os.Process.killProcess(android.os.Process.myPid());
 	    super.onDestroy();
 	}
+	
+	/*private void noti(){
+		//Contenido de notificacion
+		Notification notificacion = new Notification( 
+                R.drawable.metrans_logo,
+                "Metrans inicializando... Bienvenido",
+                System.currentTimeMillis() );
+		//Fin de contenido de la notificacion
+		PendingIntent intencionPendiente = PendingIntent.getActivity(
+		          this, 0, new Intent(this, MainActivity.class), 0);
+		notificacion.setLatestEventInfo(this, "Por favor espera",
+		       "información adicional", intencionPendiente);
+	
+		 nm.notify(ID_NOTIFICACION_CREAR, notificacion); //Enviamos la notificacion al status bar
+		
+	}*/
 	
 
 	
--- src/com/theradikalsoftware/metrans/SplashScreen.java
+++ src/com/theradikalsoftware/metrans/SplashScreen.java
@@ -2,20 +2,29 @@ package com.theradikalsoftware.metrans;
 
 import android.os.Bundle;
 import android.app.Activity;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.view.Menu;
+
 import java.util.Timer;
 import java.util.TimerTask;
+
 import android.content.Intent;
 
 
 public class SplashScreen extends Activity {
+	
+	public NotificationManager nm1; //Variables al admin de notif.
+	public static final int ID_NOTIFICACION_CREAR = 1;
 
 private long splashDelay = 6000; //milisegundos -> 	6 segundos
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_splash_screen);
-	
+		nm1 = (NotificationManager)getSystemService(NOTIFICATION_SERVICE); //Declaracion de notificacion
+		
 		TimerTask task = new TimerTask(){
 		 @Override
 	      public void run() {
@@ -26,6 +35,7 @@ private long splashDelay = 6000; //milisegundos -> 	6 segundos
 	};
 	Timer timer = new Timer();
 	timer.schedule(task, splashDelay); //Pasando los 6 segundos entramos a la tarea en si
+	noti1();
 	}
 
 	@Override
@@ -33,6 +43,23 @@ private long splashDelay = 6000; //milisegundos -> 	6 segundos
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.splash_screen, menu);
 		return true;
+	}
+/***************Funcion de notificacion en status bar ***********************/
+	private void noti1(){
+		/*Contenido de notificacion*/
+		Notification notificacion = new Notification( 
+                R.drawable.metrans_logo,
+                "Metrans inicializando... Bienvenido",
+                System.currentTimeMillis() );
+		/*Fin de contenido de la notificacion*/
+		PendingIntent intencionPendiente = PendingIntent.getActivity(
+		          this, 0, new Intent(this, MainActivity.class), 0);
+		notificacion.setLatestEventInfo(this, "Somos tan chingones",
+		       "que tenemos notificaciones aqui", intencionPendiente);
+	
+		 nm1.notify(ID_NOTIFICACION_CREAR, notificacion); //Enviamos la notificacion al status bar
+		notificacion.defaults |= Notification.DEFAULT_VIBRATE; //Agregando algo de vibracion... Espero y funcione
+		
 	}
 
 }
