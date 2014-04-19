ed6398fc87de2739a11342aa9098724c52bcd42d
TheRadikalStyle <radikal_david@hotmail.com>
2014-04-19 01:01:56 -0500

    Mejoras
    Mejor interfaz en sección acerca de
    Comienzo de la integración de los procesos primordiales

--- src/com/theradikalsoftware/metrans/AboutMe.java
+++ src/com/theradikalsoftware/metrans/AboutMe.java
@@ -4,25 +4,57 @@ import android.support.v7.app.ActionBarActivity;
 import android.support.v7.app.ActionBar;
 import android.support.v4.app.Fragment;
 import android.app.Activity;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.os.Build;
 
-public class AboutMe extends Activity{
+public class AboutMe extends Activity {
 
+	//Button cerr;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_about_me);
+		
+		//cerr = (Button)findViewById(R.id.cerrar_about);
 
 		/*if (savedInstanceState == null) {
 			getSupportFragmentManager().beginTransaction()
 					.add(R.id.container, new PlaceholderFragment()).commit();
 		}*/
+		
+		ImageView img = (ImageView)findViewById(R.id.fb_david);
+		img.setOnClickListener(new View.OnClickListener(){
+		    public void onClick(View v){
+		        Intent intent = new Intent();
+		        intent.setAction(Intent.ACTION_VIEW);
+		        intent.addCategory(Intent.CATEGORY_BROWSABLE);
+		        intent.setData(Uri.parse("http://facebook.com/radikal.david"));
+		        startActivity(intent);
+		    }
+		});
+		
+		/*ImageView img0 = (ImageView)findViewById(R.id.tw_david);
+		img.setOnClickListener(new View.OnClickListener(){
+		    public void onClick(View v0){
+		        Intent intent0 = new Intent();
+		        intent0.setAction(Intent.ACTION_VIEW);
+		        intent0.addCategory(Intent.CATEGORY_BROWSABLE);
+		        intent0.setData(Uri.parse("http://www.twitter.com/theradikalstyle"));
+		        startActivity(intent0);
+		    }
+		});*/
+		
 	}
 
 	@Override
@@ -61,5 +93,11 @@ public class AboutMe extends Activity{
 			return rootView;
 		}
 	}
+	
+	public void Cerrar(View v){
+		finish();
+	}
+
+
 
 }
